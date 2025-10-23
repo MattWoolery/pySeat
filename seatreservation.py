f76d910b8560ba73cb7c34b324cec3a82f09edde
Matt Woolery <43083781+MattWoolery@users.noreply.github.com>
2025-10-23 01:12:06 -0500

    update1
    added check if seat already exists, added a pause after displaying seatlist

--- seatreservation.py
+++ seatreservation.py
@@ -14,14 +14,22 @@ while loop:
     choice = input("Enter your choice [1-4]: ")
     choice=int(choice)
     if choice == 1:
-        seat = input("Which Seat?: ")
+        seat = input("Which Seat?: ").lower()
+        while seat in seatlist:  # Check if the seat is already reserved
+            print(f"Seat {seat} is already occupied. Please choose another seat.")
+            seat = input("Which Seat?: ").lower()
         name = input("What name?:  ")
         seatlist.update({seat:name})
     elif choice == 2:
-        seat= input("ok, which seat to remove?:  ")
+        seat= input("ok, which seat to remove?:  ").lower()
+        if seat in seatlist:
+            del seatlist[seat]
+            print(f"Seat {seat} has been removed.")
         seatlist.pop(seat)
     elif choice == 3:
-        print(seatlist)
+        for seat, name in seatlist.items():
+            print(f"{seat:10} {name}")
+        input("enter to continue")
     elif choice == 4:
         print("Goodbye")
         loop=False
