203f3401445715a51cbe85e0374afd4f179a61ee
Matt Woolery <43083781+MattWoolery@users.noreply.github.com>
2025-10-23 23:40:38 -0500

    v3
    added input validation

--- seatreservation.py
+++ seatreservation.py
@@ -1,4 +1,5 @@
 seatlist = {}
+MAX_SEATS = 99
 
 def print_menu():
     print (30 * "-" , "Menu", 30 * "-")
@@ -14,20 +15,43 @@ while loop:
     choice = input("Enter your choice [1-4]: ")
     choice=int(choice)
     if choice == 1:
-        seat = input("Which Seat?: ").lower()
-        while seat in seatlist:  # Check if the seat is already reserved
-            print(f"Seat {seat} is already occupied. Please choose another seat.")
-            seat = input("Which Seat?: ").lower()
-        name = input("What name?:  ")
-        seatlist.update({seat:name})
+        while True:
+            user_input = input(f"Enter a seat number between 1 and {MAX_SEATS}: ")
+            if user_input.isdigit():
+                seat_num = int(user_input)
+            if not user_input.isdigit():
+                print(f"Enter valid seat number between 1 and {MAX_SEATS}")
+                continue
+            if not (1 <= seat_num <= MAX_SEATS):
+                print(f"Enter valid seat number between 1 and {MAX_SEATS}")
+                continue
+            if seat_num in seatlist:
+                print(f"Seat {seat_num} is not available.  Choose another")
+                continue
+
+            print(F"Seat {seat_num} selected,")
+            name = input("What name?:  ")
+            seatlist.update({seat_num:name})
+            break
     elif choice == 2:
-        seat= input("ok, which seat to remove?:  ").lower()
-        if seat in seatlist:
-            del seatlist[seat]
-            print(f"Seat {seat} has been removed.")
-        seatlist.pop(seat)
+        user_input = input("ok, which seat to remove?:  ").strip()
+
+        try:
+            seat_num = int(user_input)
+        except ValueError:
+            print("Invalid input - please enter a valid seat nbumber.")
+        else:
+
+            if seat_num in seatlist:
+                name = seatlist[seat_num]
+                confirm = input(f"Seat {seat_num} is assigned to {name}.  remove it (y/n)").strip().lower()
+                if confirm == 'y':
+                    del seatlist[seat_num]
+                    print(f"Seat {seat_num} has been removed.")
+        
     elif choice == 3:
         for seat, name in seatlist.items():
+
             print(f"{seat:10} {name}")
         input("enter to continue")
     elif choice == 4:
